2fe20b8be9c1f408cc3e6a195b45d9b94dc414ac
Mahak0308 <mahakgupta0308@gmail.com>
2026-01-05 00:23:54 +0530

    fixed linting

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
+
+
 def home():
     return jsonify({
         "message": "Hello from CI/CD Pipeline with SemVer",
         "status": "success"
     })
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
